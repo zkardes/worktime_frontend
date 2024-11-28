6646f277356e380d708e4e266aee81e991850f9f
zkardes <zkardes64@gmail.com>
2024-11-28 09:57:39 +0100

    DeleteTemplate

---
+++ worktime_frontend/src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Login = (props) => {
+    const {loggedIn, email} = props
+    const navigate = useNavigate()
+    
+}

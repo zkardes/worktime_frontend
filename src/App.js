f364479c07c2536233c5207c92c3d0446576e03d
zkardes <zkardes64@gmail.com>
2024-12-17 18:03:20 +0100

    Registration Process finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import { useEffect, useState } from 'react';
+import {useState, useEffect } from 'react';
 import './App.css';
+//import Dashboard from './pages/Dashboard';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
 
   const [loggedIn, setLoggedIn] = useState(false)
   const [email, setEmail] = useState('')
+  const createUser =  async (user) => {
+          const requestBody = user
+
+          const result = await fetch("http://127.0.0.1:8080/api/auth/register", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+          }) 
+        } 
+  
 
   return (
    <div className="App">
@@ -19,6 +33,7 @@ function App() {
             element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
           />
           <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/register" element={<RegisterPage setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
         </Routes>
       </BrowserRouter>
     </div>)
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,6 +1,97 @@
-function RegisterPage(){
-    return 
-    
-}
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const RegisterPage = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const navigate = useNavigate();
+  let body = {};
+
+  const handleSubmit = () => {
+    fetch("http://127.0.0.1:8080/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: body,
+    }).then((res) => {
+      console.log("New User added! : " + res);
+    });
+  };
+
+  const onButtonClick = () => {
+    // Set initial error values to empty
+
+    setEmailError("");
+    setPasswordError("");
+
+    // Check if the user has entered both fields correctly
+    if ("" === email) {
+      setEmailError("Please enter your email");
+      return;
+    }
+
+    if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError("Please enter a valid email");
+      return;
+    }
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
+    if (password.length < 7) {
+      setPasswordError("The password must be 8 characters or longer");
+      return;
+    }
+    body = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    handleSubmit();
+    // Authentication calls will be made here...
+    navigate("/login");
+  };
+
+  return (
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
+        <div>Registrierung</div>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={email}
+          placeholder="Enter your email here"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={password}
+          placeholder="Enter your password here!"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Registrieren"}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default RegisterPage;
